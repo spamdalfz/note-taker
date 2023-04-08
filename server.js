9974c0e42d0e87c948d6caaac05ed9f9473596d6
spamdalfz <coreyvasser@gmail.com>
2023-04-08 01:21:29 -0700

    refactored server routes to promises.

--- server.js
+++ server.js
@@ -1,45 +1,56 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs/promises');
+const path = require('path')
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware //
+// Set up middleware to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); // Use the 'public' folder for static files
 
 // Routes //
 app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + '/public/notes.html');
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
+
+
+app.get('/api/notes', async (req, res) => {
+    try {
+        const data = await fs.readFile('./db/db.json', 'utf8');
+        const notes = JSON.parse(data);
+        res.json(notes);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error reading notes');
+    }
+});
+
+app.post('/api/notes', async (req, res) => {
+    try {
+        const data = await fs.readFile('./db/db.json', 'utf8');
+        const notes = JSON.parse(data);
+        const newNote = {
+            ...req.body,
+            id: Date.now().toString(),
+        };
+        notes.push(newNote);
+        await fs.writeFile(
+            './db/db.json',
+            JSON.stringify(notes, null, 2)
+        );
+        res.json(newNote);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
 });
 
 // Catch-all for any unmatched GET requests 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => { // Use './db/db.json' as the file path
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
-});
-
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        newNote.id = uuidv4(); // Use the uuidv4 function to generate a unique ID
-        notes.push(newNote);
-
-        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
