f517b51cb64ed2f21de2e14d71e222b2a9626cb0
spamdalfz <coreyvasser@gmail.com>
2023-04-08 00:18:28 -0700

    added middleware and server start to server.js

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public')); // Use the 'public' folder for static files
+
+// Routes
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
