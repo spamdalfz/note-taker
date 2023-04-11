52a5fda766c72135065206ac9e5f06fdd14af01c
spamdalfz <coreyvasser@gmail.com>
2023-04-10 17:07:52 -0700

    added delete route to server.js

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); // Use the 'public' folder for static files
 
+
 // Routes //
+
+// Serve the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
-
+// Get all notes
 app.get('/api/notes', async (req, res) => {
     try {
         const data = await fs.readFile('./db/db.json', 'utf8');
@@ -28,6 +31,7 @@ app.get('/api/notes', async (req, res) => {
     }
 });
 
+// Create a new note
 app.post('/api/notes', async (req, res) => {
     try {
         const data = await fs.readFile('./db/db.json', 'utf8');
@@ -48,13 +52,33 @@ app.post('/api/notes', async (req, res) => {
     }
 });
 
+// Delete a note based on its id
+app.delete('/api/notes/:id', async (req, res) => {
+    try {
+        const data = await fs.readFile('./db/db.json', 'utf8');
+        const notes = JSON.parse(data);
+        const filteredNotes = notes.filter(note => note.id !== req.params.id);
+        if (notes.length === filteredNotes.length) {
+            // If no note was deleted, return a 404 status code
+            return res.status(404).json({ message: 'Note not found' });
+        }
+        await fs.writeFile(
+            './db/db.json',
+            JSON.stringify(filteredNotes, null, 2)
+        );
+        res.status(204).end();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 // Catch-all for any unmatched GET requests 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
